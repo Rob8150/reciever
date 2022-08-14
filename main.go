3b44cb07b1c569ed0f4b6c216203597327860af3
Rob8150 <robert.clark.1967@gmail.com>
2022-08-15 08:10:29 +0930

    simple reciever and pointer reciever in golang

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Person struct {
+	id   string
+	name string
+	age  int
+	sex  string
+}
+
+type Employee struct {
+	title string
+}
+
+func (p Person) disp(job string) {
+	fmt.Println(p.id)
+	fmt.Println(p.name)
+	fmt.Println(p.age)
+	fmt.Println(p.sex)
+	fmt.Println(p.name + " is employed as " + job + " in the " + p.sex + " gender section!")
+	//return "---------"
+}
+
+func (e Employee) toLower() string {
+	return strings.ToLower(e.title)
+}
+
+func main() {
+	fmt.Println("Hello World")
+	p := Person{
+		id:   "23989",
+		name: "Robert",
+		age:  55,
+		sex:  "male",
+	}
+	e := Employee{
+		title: "Park land ranger",
+	}
+
+	p.disp((e.toLower()))
+
+}
